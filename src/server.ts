37cad3b26921a29d4f0059d0efef9540e8538fb9
mdsifat <ibnsifat@gmail.com>
2023-06-04 11:22:23 +0600

    update lint setup

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,13 @@ import app from './app'
 async function boostrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`🛢Database is connected successfully`)
+    // console.log(`🛢Database is connected successfully`)
 
     app.listen(config.port, () => {
-      console.log(`Application  listening on port ${config.port}`)
+      // console.log(`Application  listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('Failed to connect database', err)
+    // console.log('Failed to connect database', err)
   }
 }
 
