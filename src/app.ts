be4b4abda5dbe0f83168404b1039f7d3f62e88c0
mdsifat <ibnsifat@gmail.com>
2023-06-04 11:59:06 +0600

    basic logger setup

--- src/app.ts
+++ src/app.ts
@@ -14,3 +14,8 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 export default app
+
+// install winston
+// copy the logger code block
+// add the line with console to see the logs in the console
+// create separate folder for success and error logs
---
+++ src/shared/logger.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+import winston from 'winston'
+import path from 'path'
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
+      level: 'info',
+    }),
+  ],
+})
+const errorlogger = winston.createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
+    }),
+  ],
+})
+
+export { logger, errorlogger }
