2643fbaa15b5c82a7335cd3ab73f049c00b0d1e2
sifat18 <ibnsifat@gmail.com>
2023-06-07 20:13:56 +0200

    error handler setup

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,23 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1/user', userRouter)
 
+// custom error Handler class
+
+class APIError extends Error {
+  constructor(
+    public statusCode: number,
+    public message: string | undefine,
+    stack: ''
+  ) {
+    if(stack;)
+    this.stack=stack;
+    
+    
+  }else{
+    Error.captureStackTrace(this,this?.constructor)
+  }
+}
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
