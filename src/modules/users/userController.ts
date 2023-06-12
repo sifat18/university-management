c156afd4d6896067eade10429786a3e3ca55b04c
mdsifat <ibnsifat@gmail.com>
2023-06-12 12:10:41 +0600

    zod_basic setup done

--- src/modules/users/userController.ts
+++ src/modules/users/userController.ts
@@ -1,8 +1,9 @@
 import { RequestHandler } from 'express'
 import { createUserDB } from './userService'
-
 export const createUser: RequestHandler = async (req, res, next) => {
   try {
+    // since asyn process so use parseAsync
+
     const { user } = req.body
     const result = await createUserDB(user)
     res.status(200).json({
